1f2683f36283adbe3be678f1bb6a020c5ded6fb3
Geektech <development.geektech@gmail.com>
2024-06-13 16:25:27 +0530

    jks file and cerdentionals

---
+++ android/app/src/main/kotlin/com/arspace_co/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arspace_co
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/util/environment.dart
+++ lib/util/environment.dart
@@ -1,5 +1,5 @@
 class Environments {
-  static const String appName = 'Arspace';
+  static const String appName = 'Arspace.co';
   static const String companyName = 'Arspace';
   //static const String googleMapsKey = 'AIzaSyAIkNfQlM2dYsCTc7ut66ztA7osmBt1JgY';
   static const String apiBaseURL = 'http://34.121.97.111/api/v1/';
